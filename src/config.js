e9ba04857a073f36aa4e46ece1b9e4226b9aee0a
Michael Liang <michaelpliang@gmail.com>
2015-11-10 22:52:14 -0800

    fixed github link

--- src/config.js
+++ src/config.js
@@ -3,32 +3,32 @@ app.config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
       url: '/',
-      templateUrl: './pages/home.html',
+      templateUrl: './src/pages/home.html',
       controller: 'mainController'
     })
     .state('add', {
       url: '/add',
-      templateUrl: './pages/add.html',
+      templateUrl: './src/pages/add.html',
       controller: 'addController'
     })
     .state('animals', {
       url: '/animals',
-      templateUrl: './pages/animals.html',
+      templateUrl: './src/pages/animals.html',
       controller: 'animalController'
     })
     .state('addTrainers', {
       url: '/addTrainers',
-      templateUrl: './pages/addTrainers.html',
+      templateUrl: './src/pages/addTrainers.html',
       controller: 'addTrainerController'
     })
     .state('trainers', {
       url: '/trainers',
-      templateUrl: './pages/trainers.html',
+      templateUrl: './src/pages/trainers.html',
       controller: 'trainerController'
     })
     .state('trainerShow', {
       url: '/trainers/:trainerId',
-      templateUrl: './pages/show.html',
+      templateUrl: './src/pages/show.html',
       controller: 'showController'
     })
 
