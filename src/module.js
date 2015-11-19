22ff3e034bf1a5488a0e7623e478d96f492ea9f3
Michael Liang <michaelpliang@gmail.com>
2015-11-18 17:13:38 -0800

    styling update

--- src/module.js
+++ src/module.js
@@ -1,3 +1,3 @@
 window.app = angular.module('animalApp', ['ui.router']);
-// window.url = 'https://pokemon-shelter.herokuapp.com/';
-window.url = 'http://localhost:3000/';
+window.url = 'https://pokemon-shelter.herokuapp.com/';
+// window.url = 'http://localhost:3000/';
